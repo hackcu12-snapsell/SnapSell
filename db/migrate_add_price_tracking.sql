2601b9a000b0384b0088f7b701600ac5b7289423
Brandon Reeves <brre8838@colorado.edu>
2026-03-08 11:40:43 -0600

    seed data

--- db/migrate_add_price_tracking.sql
+++
@@ -1,2 +0,0 @@
--- Migration: add last_price_change_date to items
-ALTER TABLE items ADD COLUMN IF NOT EXISTS last_price_change_date TIMESTAMP;
--- db/migrate_add_purchase_price.sql
+++
@@ -1,2 +0,0 @@
--- Migration: add purchase_price (what we paid) to items
-ALTER TABLE items ADD COLUMN IF NOT EXISTS purchase_price NUMERIC DEFAULT 0;
